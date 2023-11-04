f63e867a5521f8063a854fa57857691f4556e77a
Lakanna <annalakus@ukr.net>
2023-11-04 19:21:19 +0000

    try

--- src/index.js
+++ src/index.js
@@ -1,44 +1,113 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css"
-// Notiflix.Notify.info('Cogito ergo sum');
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
 
 
 const elem = {
     form: document.querySelector(".search-form"),
-    loadMore: document.querySelector("button"),
+    loadMore: document.querySelector(".load-more"),
     gallery: document.querySelector(".gallery")
 };
 
+let page = 12;
+let searchElem = "";
 
 
 elem.form.addEventListener("submit", handlerSearch);
+elem.loadMore.addEventListener('click', handlerLoadMore);
+
+
+
 
 async function handlerSearch(evt) {
 
-    evt.preventDefault();
-    const searchElem = evt.target.elements.searchQuery.value;
+  evt.preventDefault();
+   
+  searchElem = evt.target.elements.searchQuery.value;
 
-    const images = await fetchImages(searchElem);
+  elem.gallery.innerHTML = "";
 
-    if (images.length === 0) {
-        Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
-        return;
-    }
-    
-    console.log(images);
-    elem.gallery.innerHTML = "";
+  page = 1;
 
+  try {
+      const response = await fetchImages(searchElem, page);
+
+    if (response.hits.length === 0) {
+        
+      Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
+      
+      return;
+      
+       }
+  
+      const images = response.hits;
+          
     elem.gallery.insertAdjacentHTML("beforeend", createMarcup(images));
 
-    const lightbox = new SimpleLightbox('.gallery a', { });
+    const lightbox = new SimpleLightbox('.gallery a', {});
 
+    evt.target.reset();
+  
+    if (page >= response.totalHits / 40) {
+                  
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      
+      page = 0;
+      
+      return;
+      
+    } else { 
+
+      elem.loadMore.classList.remove("load-more-hidden")
+       
+        }
+  } catch (error) {
+    
+     Notiflix.Notify.failure('Sorry, search is currently unavailable')
+     }
+      
+  }
+
+
+
+
+async function handlerLoadMore() {
+
+  page += 1;
+    
+  try {
+    const response = await fetchImages(searchElem, page);
+
+      if (response.hits.length === 0) {
+        Notiflix.Notify.info('Sorry, there are no images matching your search query. Please try again.')
+        return;
+       }
+  
+    const images = response.hits;
+          
+    elem.gallery.insertAdjacentHTML("beforeend", createMarcup(images));
+
+    const lightbox = new SimpleLightbox('.gallery a', {});
+
+    lightbox.refresh();
+  
+    if (page >= response.totalHits / 40) {
+       
+      elem.loadMore.classList.add("load-more-hidden")
+      
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      
+         return;
+        } 
+   } catch (error) {
+       Notiflix.Notify.failure('Sorry, search is currently unavailable') }
+   
 }
+     
 
-async function fetchImages(searchElem) {
+
+async function fetchImages(searchElem, page = 1) {
     axios.defaults.baseURL = "";
     const params = {
         key: "40437222-3b8e1aead0ae08f3118e12752",
@@ -49,22 +118,16 @@ async function fetchImages(searchElem) {
         per_page: 40,
         page: 1
     };
-
-    params.q = searchElem;
-
-    const resp = await axios.get(`https://pixabay.com/api/`, { params }).then((resp)=> resp.data.hits).catch(function (error) {
-    if (error.response) {
-              console.log(error.response.status);
-          } else if (error.request) {
-          console.log(error.request);
-    } else {
-          console.log('Error', error.message);
-    }
     
-  });
+     params.q = searchElem;
+     params.page = page;
 
-    return resp;
-}
+   const resp = await axios.get(`https://pixabay.com/api/`, { params }).then((resp) => resp.data);
+   return resp; 
+  };
+
+    
+
 
 function createMarcup(arr) {
     return arr.map(({ tags, webformatURL, likes, downloads, views, largeImageURL, comments }) => `
@@ -91,4 +154,3 @@ function createMarcup(arr) {
 }
 
 
-// fetchImages("cats")
